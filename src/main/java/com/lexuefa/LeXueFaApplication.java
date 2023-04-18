51af0059235f655fdbe11751a9edc3fbcdfa06ce
lvMagui <2416053677@qq.cmo>
2023-04-18 21:25:12 +0800

    导入依赖，创建启动类

---
+++ src/main/java/com/lexuefa/LeXueFaApplication.java
@@ -0,0 +1,15 @@
+package com.lexuefa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ukir
+ * @date 2023/04/18 19:20
+ **/
+@SpringBootApplication
+public class LeXueFaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LeXueFaApplication.class);
+    }
+}
